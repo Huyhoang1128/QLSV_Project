008698624e68c8a52dcb80b3fb85be5effe48846
Hoang PH <hoangph@vietis.com.vn>
2025-02-18 16:24:05 +0700

    make view for show function

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -3,10 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Student;
+use App\Models\Classroom;
+use Illuminate\Support\Facades\DB;
 
 class StudentController extends Controller
 {
     public function index(){
-        
+        return view('students.index', ['students'=> Student::paginate(10)]);
+    }
+    public function create(){
+
+    }
+    public function store(Request $request){
+
+    }
+    public function show(Student $student){
+        return view('students.show', compact('student'));
     }
 }
--- resources/views/students/show.blade.php
+++ resources/views/students/show.blade.php
@@ -24,3 +24,18 @@
                 {{ $student->StudentEmail }}
             </div>
         </div>
+        <div class="row mb-4">
+            <label class="col-sm-2 col-label-form"><b>Gioi tinh</b></label>
+            <div class="col-sm-10">
+                @if ($student->StudentGender = 0)
+                    Nam
+                @else
+                    NULL
+                @endif
+            </div>
+                <a href="{{ route('students.index') }}" class="btn btn-secondary">Quay lai</a>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection('content')
