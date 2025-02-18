f44c1ad7cfcdc53a866984867557d5d8835c8f9f
Hoang PH <hoangph@vietis.com.vn>
2025-02-18 16:15:38 +0700

    make view

---
+++ resources/views/students/show.blade.php
@@ -0,0 +1,26 @@
+@extends('student.master')
+
+@section('content')
+
+<div class="card">
+    <div class="card-header">
+        <div class="row">
+            <div class="col col-md-6"><b>Thong tin chi tiet sinh vien</b></div>
+            <div class="col col-md-6">
+                <a href="{{ route('students.index') }}" class="btn btn-primary btn-sm float-end">Xem tat ca danh sach</a>
+            </div>
+        </div>
+    </div>
+    <div class="card-body">
+        <div class="row mb-3">
+            <label class="col-sm-2 col-label-form"><b>Ten sinh vien</b></label>
+            <div class="col-sm-10">
+                {{ $student->StudentName }}
+            </div>
+        </div>
+        <div class="row mb-3">
+            <label class="col-sm-2 col-label-form"><b>Dia chi email</b></label>
+            <div class="col-sm-10">
+                {{ $student->StudentEmail }}
+            </div>
+        </div>
