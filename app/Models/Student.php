884395753e12de66c72c533d54a24c521509a95f
Hoang PH <hoangph@vietis.com.vn>
2025-02-18 15:25:12 +0700

    add Model and Controller

---
+++ app/Models/Student.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Student extends Model
+{
+    use HasFactory;
+    public $timestamps = false;
+    protected $primarykey = 'StudentID';
+    public function classroom(){
+        return $this->belongsTo(Classroom::class, 'ClassRoomID', 'ClassRoomID');
+    }
+    protected $fillable = ['StudentID','StudentName', 'StudentEmail', 'StudentGender', 'ClassroomID'];
+}
